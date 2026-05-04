46f8115b424ce50135605feb569720c4fc825650
toufikansari <toufikmdansari@gmail.com>
2026-05-04 16:26:34 +0530

    All Factors of given numbers

---
+++ All Factors.py
@@ -0,0 +1,11 @@
+from math import sqrt
+num = 20
+result = []
+for i in range(1 , int(sqrt(num)) + 1):
+    if num % i == 0:
+        result.append(i)
+        if num // i != i:
+            result.append(num // i)
+
+result.sort()
+print(result)
